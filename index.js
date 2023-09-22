057c41e7f7b6a064eaf217f2a2edd20c32d9b0cc
DMelisena <demonlordx777@gmail.com>
2023-09-23 00:36:02 +0700

    Bug on player selection won't change when clicking another button

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+//Function for opponent's choice of attack
 function getComputerChoice(){
     computerChoiceArr=["rock","paper","scissor"]
     result=computerChoiceArr[Math.floor(Math.random()*computerChoiceArr.length)]
     return result
 }
 
+//choices selection variables
 let playerSelection=prompt(" What would you use? rock, paper or scissor?")
 let computerSelection=getComputerChoice()
 
-winning =`The Opponent use ${computerSelection}, You won! ${playerSelection} beats ${computerSelection} `
-draw =`Both of you used ${playerSelection}, It's a tie`
-losing = `The Opponent use ${computerSelection}, You lose! ${computerSelection} beats ${playerSelection}`
+//Game's output
+winning =`You used ${playerSelection},The Opponent use ${computerSelection}, You won! ${playerSelection} beats ${computerSelection} `
+draw =`You used ${playerSelection},Both of you used ${playerSelection}, It's a tie`
+losing = `You used ${playerSelection},The Opponent use ${computerSelection}, You lose! ${computerSelection} beats ${playerSelection}`
 function playRound(playerSelection,computerSelection){
     if (computerSelection=="scissor" && playerSelection=="rock"){
         return winning
@@ -27,15 +30,19 @@ function playRound(playerSelection,computerSelection){
         return losing
      }
 }
-console.log(computerSelection,playerSelection)
-console.log(playRound(playerSelection,computerSelection))
-
-function game(){
-    for (i=0;i<5;i++){
-        console.log(i)
-        let playerSelection=prompt(" What would you use? rock, paper or scissor?").toLowerCase()
-        console.log(playRound(playerSelection,computerSelection))
-        computerSelection=getComputerChoice()
-    }
+//Show on console log what's going on
+//console.log(computerSelection,playerSelection)
+//console.log(playRound(playerSelection,computerSelection))
+use=''
+//console to run when opening the web
+function game(use){
+    //let playerSelection=prompt(" What would you use? rock, paper or scissor?").toLowerCase()
+    let playerSelection=use.toLowerCase()
+    computerSelection=getComputerChoice()
+    console.log(computerSelection)
+    console.log(playRound(playerSelection,computerSelection))
 }
-game()
+
+document.getElementById("rockButton").addEventListener("click",function(){game("rock")})
+document.getElementById("paperButton").addEventListener("click",function(){game("paper")})
+document.getElementById("scissorButton").addEventListener("click",function(){game("scissor")})
