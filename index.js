f33ec764f71ade71fb471e900853c805ea21b789
DMelisena <demonlordx777@gmail.com>
2024-04-28 06:02:12 +0700

    Adding function to play 5 games and state the results and score

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ function getHumanChoice(){
   myChoice=window.prompt("pick R, S, or P","R");
   return myChoice
 }
-
+let humanScore=0;
+let computerScore=0;
 function playRound(humanChoice,computerChoice){
   if (humanChoice==computerChoice){
     console.log("draw");
@@ -33,28 +34,59 @@ function playRound(humanChoice,computerChoice){
   else if (humanChoice=="R"){
       if (computerChoice=="S"){
         console.log("You Win")
+        humanScore+=1
         }
       else{
         console.log("You Lose")
+        computerScore+=1
         }
   }
   else if (humanChoice=="S"){
       if (computerChoice=="P"){
         console.log("You Win")
+        humanScore+=1
         }
       else{
         console.log("You Lose")
+        computerScore+=1
         }
   }
   else {
       if (computerChoice=="R"){
         console.log("You Win")
+        humanScore+=1
         }
       else{
         console.log("You Lose")
+        computerScore+=1
         }
   }
 }
-  const human = getHumanChoice();
-  const computer = getComputerChoice();
+function playGame(){
+  
+  let human = getHumanChoice();
+  let computer = getComputerChoice();
   playRound(human,computer)
+  human = getHumanChoice();
+  computer = getComputerChoice();
+  playRound(human,computer)
+  human = getHumanChoice();
+  computer = getComputerChoice();
+  playRound(human,computer)
+  human = getHumanChoice();
+  computer = getComputerChoice();
+  playRound(human,computer)
+  human = getHumanChoice();
+  computer = getComputerChoice();
+  playRound(human,computer)
+  if(humanScore>computerScore){
+    console.log("You Won, the score are "+humanScore+" - "+computerScore)
+  }
+  else if(computerScore>humanScore){
+    console.log("You lose, the score are "+humanScore+" - "+computerScore)
+  }
+  else{
+    console.log("No body wins, or.. everyone is a winner!! It's "+computerScore+" - "+humanScore)
+  }
+}
+playGame()
